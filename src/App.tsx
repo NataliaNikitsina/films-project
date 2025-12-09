0471ab56718b0040a5fa0f2a556477d42dcc28c4
nataliaN <asdfghmax@yandex.ru>
2025-12-09 18:47:04 +0300

    "first commit"

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Header} from "./common/components/Header/Header.tsx";
+import {Footer} from "./common/components/Footer/Footer.tsx";
+
+export function App() {
+
+  return (
+    <>
+        <Header />
+        <Footer/>
+    </>
+  )
+}
+
