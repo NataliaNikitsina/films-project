f401c311170e067f13506ccd2fc48e06f8f22133
nataliaN <asdfghmax@yandex.ru>
2026-01-03 22:02:14 +0300

    refactor: add pagination on search page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,21 +1,36 @@
 import "./SearchPage.module.css"
 import { selectSearchValue, useSearchMoviesQuery } from "@/app/model";
-import {useAppSelector} from "@/common/hooks";
-import {MoviesList, SearchForm, SkeletonMovies} from "@/common/components";
+import { useAppSelector } from "@/common/hooks";
+import { MoviesList, Pagination, SearchForm, SkeletonMovies } from "@/common/components";
+import { useEffect, useState } from "react";
 
 export function SearchPage() {
-    const searchValue = useAppSelector(selectSearchValue)
-    const {data, isLoading} = useSearchMoviesQuery({query: searchValue}, {skip: !searchValue})
+  const [currentPage, setCurrentPage] = useState<number>(1)
 
-    return (
-        <section>
+  const searchValue = useAppSelector(selectSearchValue)
+  const {data, isLoading} = useSearchMoviesQuery({query: searchValue, page: currentPage}, {skip: !searchValue})
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [data])
+
+  return (
+          <section>
             <h2>Search Results</h2>
             <SearchForm/>
             {isLoading && <SkeletonMovies/>}
             {!searchValue && <p>Enter a movie title to start searching...</p>}
-            {data?.results.length === 0 && <p>No matches found for {`"${searchValue}"`}</p>}
-            {data && searchValue && <MoviesList movies={data ? data.results : []} />}
-        </section>
-    )
+            {data?.results.length === 0 && searchValue && <p>No matches found for {`"${searchValue}"`}</p>}
+            {data && searchValue && <>
+              <MoviesList movies={data.results}/>
+              <Pagination
+                      currentPage={currentPage}
+                      setCurrentPage={setCurrentPage}
+                      pagesCount={data.total_pages || 1}
+              />
+            </>}
+
+          </section>
+  )
 }
 
