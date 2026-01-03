594400f69f27ab7d17bbd245de74ae0522aef96d
nataliaN <asdfghmax@yandex.ru>
2026-01-03 21:27:53 +0300

    refactor: decomposition actors

--- src/pages/MoviePage/MovieActors/MovieActors.tsx
+++ src/pages/MoviePage/MovieActors/MovieActors.tsx
@@ -1,8 +1,6 @@
 import s from './MovieActors.module.css'
 import type { Cast } from "@/common/types";
-import { IMAGE_SIZES } from "@/common/constants";
-import noCover from "@/assets/images/noCover.svg"
-import { useImageBasePath } from "@/common/hooks/useImageBasePath.ts";
+import { Actor } from "@/pages/MoviePage/MovieActors/Actor";
 
 
 type Props = {
@@ -10,24 +8,11 @@ type Props = {
 }
 
 export const MovieActors = ({cast}: Props) => {
-  const imageBasePath = useImageBasePath(IMAGE_SIZES.CARD)
   return (
           <div className={s.container}>
             <h2>Cast</h2>
             <div className={s.actors}>
-              {cast.map(el => (
-                      <div className={s.actor}>
-                        <div className={s.photo}>
-                          <img src={el.profile_path ? imageBasePath + el.profile_path : noCover}
-                               alt="avatar1"/>
-                        </div>
-                        <div className={s.info}>
-                          <p>{el.name}</p>
-                          <p className={s.role}>Role: {el.character}</p>
-                        </div>
-
-                      </div>
-              ))}
+              {cast.map(actor => (<Actor actor={actor}/>))}
             </div>
           </div>
   )
